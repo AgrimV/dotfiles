98908c94896fa8b1e463802e044819382370ad26
Agrim Vats <agrimvats10@gmail.com>
2025-09-02 12:37:35 +0530

    prelude to hyprland

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,2 +1,4 @@
 vim.lsp.enable("lua_ls") -- lua-language-server
 vim.lsp.enable("pylsp") -- python-lsp-server
+vim.lsp.enable("qmlls") -- qml language server
+vim.lsp.enable("hyprls") -- hypr conf language server (requires golang)
