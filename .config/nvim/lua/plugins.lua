b050620d9b8d3350915df190dd26a87ba9b7a9fc
Agrim Vats <agrimvats10@gmail.com>
2025-12-02 22:16:58 +0530

    replace fish with moon

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -108,16 +108,10 @@ require("lazy").setup({
     },
     {
       "mason-org/mason.nvim",
-      opts = {},
-    },
-    {
-      "mason-org/mason-lspconfig.nvim",
-      opts = {},
-    },
-    {
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-      opts = {
-        ensure_installed = {
+      config = function()
+        require("mason").setup()
+
+        local ensure_installed = {
           -- LSP
           "hyprls",
           "lua-language-server",
@@ -128,8 +122,23 @@ require("lazy").setup({
           -- Formatter
           "prettier",
           "stylua",
-        },
-      },
+        }
+
+        local installed_package_names = require("mason-registry").get_installed_package_names()
+        for _, v in ipairs(ensure_installed) do
+          if not vim.tbl_contains(installed_package_names, v) then
+            vim.cmd(":MasonInstall " .. v)
+          end
+        end
+
+        local installed_packages = require("mason-registry").get_installed_packages()
+        local installed_lsp_names = vim.iter(installed_packages):fold({}, function(acc, pack)
+          table.insert(acc, pack.spec.neovim and pack.spec.neovim.lspconfig)
+          return acc
+        end)
+
+        vim.lsp.enable(installed_lsp_names)
+      end,
     },
     {
       "stevearc/conform.nvim",
